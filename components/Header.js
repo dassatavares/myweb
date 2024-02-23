81376f07ac6841f9c07fc88b90a2af1a95600fbc
Dassaev Tavares <dassaevtavares@gmail.com>
2024-02-22 23:34:37 -0300

    pro pc

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import styles from '../styles/Home.module.scss';
+
+export default function Header() {
+
+  return (
+    <>
+
+      <header className={styles.header}>
+        <div>
+             
+        </div>
+        <ul>
+            <li>Sobre</li>
+            <li>Habilidades</li>
+            <li>Projetos</li>
+            <li>Contato</li>
+        </ul>
+      </header>
+
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,108 +1,51 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.scss';
+import Header from '../components/Header';
 
 export default function Home() {
+
+  useEffect(() => {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach((section, index) => {
+      const level = section.closest('.level');
+
+      if (level) {
+        const bullet = document.createElement("div");
+        bullet.className = 'bullet-' + index;
+
+        level.appendChild(bullet);
+
+        console.log(bullet);
+      }
+    });
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Header />
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section className={styles.mainContainer}>
+        <div className={styles.mainText}>
+          <h1>Build your next idea even faster.</h1>
+          <p>Beautifully designed, expertly crafted components and templates, built by the makers of Tailwind CSS. The perfect starting point for your next project.</p>
+          <button>More</button>
         </div>
-      </main>
+      </section>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
+      <section className={styles.mainContainer}>
+        <div className={styles.mainText}>
+          <h1>Build your next idea even faster.</h1>
+          <p>Beautifully designed, expertly crafted components and templates, built by the makers of Tailwind CSS. The perfect starting point for your next project.</p>
+          <button>More</button>
+        </div>
+      </section>
+      
+      <div className={styles.level}>
+        <div className={styles.bullet}>
+        </div>
+      </div>
 
       <style jsx global>{`
         html,
@@ -125,7 +68,10 @@ export default function Home() {
         * {
           box-sizing: border-box;
         }
+        ul, li{
+          list-style-type: none;
+        }
       `}</style>
-    </div>
+    </>
   );
 }
